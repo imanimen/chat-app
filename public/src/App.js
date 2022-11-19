f8995ffceaa3d8d02621d053361b4e094c51a0a9
Iman Imen <imanimen81@gmail.com>
2022-11-19 19:07:43 +0330

    FIX app

--- public/src/App.js
+++ public/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from "./pages/Register";
 import Login from './pages/Login';
+import Chat from './pages/Chat'
 export default function App() {
   return (
     <BrowserRouter>
@@ -9,7 +10,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         {/* <Route path="/setAvatar" element={<SetAvatar />} /> */}
-        {/* <Route path="/" element={<Chat />} /> */}
+        <Route path="/" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
