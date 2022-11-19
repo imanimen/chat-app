a4f3f2cd9ee2a3bcd6cb102c3381c635561eb60a
Iman Imen <imanimen81@gmail.com>
2022-11-19 15:57:56 +0330

    ADD files

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL, () => {
+    newUrlParser:true;
+    newUnifiedTopology:true;
+}).then(() => {
+    console.log("DB connection successfull")
+}).catch((err) => {
+    console.log(err.message)
+})
+
 const server = app.listen(process.env.PORT, () => {
     console.log(`server started on port ${process.env.PORT}`)
 })
