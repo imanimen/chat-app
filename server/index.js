0341db46d5a1ef1f452908f0546f5c08775a303e
Iman Imen <imanimen81@gmail.com>
2022-11-19 15:32:41 +0330

    ADD server starter

--- server/index.js
+++ server/index.js
@@ -5,4 +5,10 @@ const mongoose = require("mongoose");
 
 const app = express();
 require("dotenv").config();
- 
+
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`server started on port ${process.env.PORT}`)
+})
